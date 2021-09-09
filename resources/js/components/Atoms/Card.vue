dcecb7d08399f89cf030f8f5cdf36721c47a1ea3
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-09 17:31:01 -0300

    Criando átomos de cartão e segmento com título

---
+++ resources/js/components/Atoms/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-white shadow-lg rounded overflow-hidden">
+        <header class="w-full py-3 px-4 text-white font-bold bg-blue-600"><slot name="header" /></header>
+
+        <div class="py-3 px-4"><slot /></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Atoms/Segment.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <div class="pl-4 pb-4 mt-2 mb-6 border-b border-gray-400">
+            <h4><slot name="title"/></h4>
+        </div>
+
+        <slot/>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
