2d6e2454e62d07f328cc06a5f127824c1920809d
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-09 17:31:34 -0300

    Criando o layout da página

---
+++ resources/js/components/Templates/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+    <aside class="bg-gray-600 text-white"><sidebar/></aside>
+
+    <header class="h-full px-24 py-3 bg-white border-b border-gray-500"><header-toolbar/></header>
+
+    <main class="bg-gray-100 p-12 h-full"><slot/></main>
+</template>
+
+<script>
+import Sidebar from '../Organisms/Sidebar.vue'
+import HeaderToolbar from '../Organisms/HeaderToolbar.vue'
+
+export default {
+  components: { Sidebar, HeaderToolbar },
+
+}
+</script>
+
+<style>
+#app {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 280px 1fr;
+    grid-template-rows: 4rem 1fr;
+}
+
+aside {
+    grid-row: 1 / -1;
+    grid-column: 1 / 2;
+}
+
+header {
+    grid-row: 1 / 2;
+    grid-column: 2 / 3;
+}
+
+main {
+    grid-row: 2 / 3;
+    grid-column: 2 / 3;
+}
+</style>
