c0a93dd216ff060a706f3573c52adfc22dc2ee21
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-10 15:04:28 -0300

    Trocando cor do cabeçalho do Card

--- resources/js/components/Atoms/Card.vue
+++ resources/js/components/Atoms/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white shadow-lg rounded overflow-hidden">
-        <header class="w-full py-3 px-4 text-white font-bold bg-blue-600"><slot name="header" /></header>
+        <header class="w-full py-3 px-4 text-white font-bold bg-indigo-600"><slot name="header" /></header>
 
         <div class="py-3 px-4"><slot /></div>
     </div>
