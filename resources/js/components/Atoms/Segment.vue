efa909574a8e9878047bab2e138a6bf659e71660
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-10 15:05:54 -0300

    Adicionando toolbar ao segment

--- resources/js/components/Atoms/Segment.vue
+++ resources/js/components/Atoms/Segment.vue
@@ -1,7 +1,11 @@
 <template>
     <section>
-        <div class="pl-4 pb-4 mt-2 mb-6 border-b border-gray-400">
+        <div class="flex flex-row items-center justify-between px-4 pb-4 mt-2 mb-6 border-b border-gray-400">
             <h4><slot name="title"/></h4>
+
+            <div>
+                <slot name="toolbar"/>
+            </div>
         </div>
 
         <slot/>
