1cf9493e2f0a9f1b2d0932072defb52e5a03b09d
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-10 15:05:44 -0300

    Corrigindo tamanho da barra lateral do cabeçalho

--- resources/js/components/Organisms/HeaderToolbar.vue
+++ resources/js/components/Organisms/HeaderToolbar.vue
@@ -4,8 +4,8 @@
 
         </div>
 
-        <div>
-            <nav class="w-2/12 flex flex-row items-center justify-around">
+        <div class="w-1/12">
+            <nav class="w-full flex flex-row items-center justify-around">
                 <div class="relative w-8 h-8 flex flex-row items-center justify-start rounded py-2 px-2 hover:bg-gray-300">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
                     <span class="bg-red-500 w-4 h-4 mb-1 ml-1 flex items-center justify-center rounded-full absolute top-0 right-0 text-xs text-white">8</span>
