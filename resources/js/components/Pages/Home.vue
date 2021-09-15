ecab3b364c61b167dc4ec1260c7adf330cdfca4d
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-15 17:28:45 -0300

    Atualizando componentes que já existiam

--- resources/js/components/Pages/Home.vue
+++ resources/js/components/Pages/Home.vue
@@ -1,6 +1,111 @@
 <template>
-    <layout>
-        <segment>
+    <Layout>
+        <Segment>
+            <template #title>Abas</template>
+
+            <section class="bg-white border border-gray-300 shadow-lg rounded overflow-hidden">               
+                <Tabs>
+                    <Tab title="Listar">
+                         <section class="bg-white border border-gray-300 rounded overflow-hidden m-3">
+                            <header class="py-3 px-4">
+                                <h3 class="font-bold text-lg text-indigo-900">Usuários</h3>
+                            </header>
+
+                            <table class="w-full">
+                                <thead class="bg-gray-100 text-gray-600 border-b border-t border-gray-300">
+                                    <tr>
+                                        <th class="text-left px-4 py-2">Cabeçalho 1</th>
+                                        <th class="text-left px-4 py-2">Cabeçalho 2</th>
+                                        <th class="text-left px-4 py-2">Cabeçalho 3</th>
+                                        <th class="text-left px-4 py-2">Ações</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr class="border-b border-t border-gray-300">
+                                        <td class="px-4 py-2 text-gray-700">Corpo 1</td>
+                                        <td class="px-4 py-2 text-gray-700">Corpo 2</td>
+                                        <td class="px-4 py-2 text-gray-700">Corpo 3</td>
+                                        <td class="px-4 py-2 text-gray-700">
+                                            <div class="flex flex-row items-center gap-2">
+                                                    <div class="flex items-center justify-center cursor-pointer rounded transition-colors duration-150 ease-in-out w-7 h-7 hover:bg-gray-200">
+                                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                                                    </div>
+                                                    <div class="flex items-center justify-center cursor-pointer rounded transition-colors duration-150 ease-in-out w-7 h-7 hover:bg-gray-200">
+                                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                                    </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    
+                                    <tr class="border-b border-t border-gray-300">
+                                            <td class="px-4 py-2 text-gray-700">Corpo 1</td>
+                                            <td class="px-4 py-2 text-gray-700">Corpo 2</td>
+                                            <td class="px-4 py-2 text-gray-700">Corpo 3</td>
+                                            <td class="px-4 py-2 text-gray-700">
+                                                <div class="flex flex-row items-center gap-2">
+                                                    <div class="flex items-center justify-center cursor-pointer rounded transition-colors duration-150 ease-in-out w-7 h-7 hover:bg-gray-200">
+                                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                                                    </div>
+                                                    <div class="flex items-center justify-center cursor-pointer rounded transition-colors duration-150 ease-in-out w-7 h-7 hover:bg-gray-200">
+                                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                    </tr>
+
+                                    <tr class="border-b border-t border-gray-300">
+                                            <td class="px-4 py-2 text-gray-700">Corpo 1</td>
+                                            <td class="px-4 py-2 text-gray-700">Corpo 2</td>
+                                            <td class="px-4 py-2 text-gray-700">Corpo 3</td>
+                                            <td class="px-4 py-2 text-gray-700">
+                                                <div class="flex flex-row items-center gap-2">
+                                                    <div class="flex items-center justify-center cursor-pointer rounded transition-colors duration-150 ease-in-out w-7 h-7 hover:bg-gray-200">
+                                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                                                    </div>
+                                                    <div class="flex items-center justify-center cursor-pointer rounded transition-colors duration-150 ease-in-out w-7 h-7 hover:bg-gray-200">
+                                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </section>
+                    </Tab>
+
+                    <Tab title="Criar">
+                          <form class="px-4 py-2 mb-2">
+                            <InputText type="text" name="item-1" id="item-1" label="Item 1" />
+
+                            <InputText type="text" name="item-2" id="item-2" label="Item 2" />
+
+                            <InputText type="text" name="item-3" id="item-3" label="Item 3" />
+
+                            <Button>
+                                Salvar
+                            </Button>
+                        </form>
+                    </Tab>
+                    
+                    <Tab title="Editar">
+                          <form class="px-4 py-2 mb-2">
+                            <InputText type="text" name="item-1" id="item-1" label="Item 1" modelValue="Corpo 1" />
+
+                            <InputText type="text" name="item-2" id="item-2" label="Item 2" modelValue="Corpo 2" />
+
+                            <InputText type="text" name="item-3" id="item-3" label="Item 3" modelValue="Corpo 3" />
+
+                            <Button>
+                                Salvar
+                            </Button>
+                        </form>
+                    </Tab>
+                </Tabs>
+            </section>
+        </Segment>
+
+        <Segment class="mt-20">
             <template #title>Título</template>
             <section class="grid grid-cols-4 gap-4">
                 <card>
@@ -24,9 +129,9 @@
                     Conteúdo
                 </card>
             </section>
-        </segment>
+        </Segment>
 
-        <segment class="mt-20">
+        <Segment class="mt-20">
             <template #title>Tabela</template>
             <template #toolbar>
                 <div class="flex flex-row items-center justify-between">
@@ -109,11 +214,10 @@
                        </tr>
                    </tbody>
                </table>
-
             </section>
-        </segment>
+        </Segment>
 
-        <segment class="mt-20">
+        <Segment class="mt-20">
             <template #title>Formulário</template>
 
             <section class="bg-white border border-gray-300 shadow-lg rounded overflow-hidden">               
@@ -150,7 +254,7 @@
                     <Checkbox id="aceite-privacidade" name="aceite-privacidade" label="Li e aceito a Política de Privacidade" />
                 </form>
             </section>
-        </segment>
+        </Segment>
 
         <Toastr v-if="toastInfo" @click="toastInfo = false" type="info" title="Você sabia?" description="Aqui está uma coisa que você pode gostar!" />
         
@@ -172,14 +276,16 @@
                 </Button>
             </template>
         </Modal>
-    </layout>
+    </Layout>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity';
 
+import Tab from '../Atoms/Tab.vue';
 import Card from '../Atoms/Card.vue';
 import List from '../Atoms/List.vue';
+import Tabs from '../Atoms/Tabs.vue';
 import Modal from '../Atoms/Modal.vue';
 import Button from '../Atoms/Button.vue';
 import Segment from '../Atoms/Segment.vue';
@@ -191,7 +297,7 @@ import InputText from '../Atoms/form/InputText.vue';
 import InputRadio from '../Atoms/form/InputRadio.vue';
 
 export default {
-    components: { List, Layout, Card, Segment, Button, Select, InputText, InputRadio, Checkbox, Modal, Toastr },
+    components: { Tab, List, Layout, Card, Segment, Button, Select, InputText, InputRadio, Checkbox, Modal, Toastr, Tabs },
     setup() {
         const toastInfo = ref(true)
         const toastForm = ref(false)
