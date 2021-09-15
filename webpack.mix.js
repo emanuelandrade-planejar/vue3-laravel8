fdf9345d83b54525ef208c0aa45bc051fa0a92a9
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-15 11:01:19 -0300

    Corrigindo configuração do BrowserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,5 @@
 const mix = require('laravel-mix');
 
-// mix.browserSync({
-//     proxy: process.env.APP_URL,
-//     port: 8001,
-//     notify: false
-// });
-
 mix.js('resources/js/app.js', 'public/js').vue();
 
 mix.postCss('resources/css/app.css', 'public/css', [
@@ -13,3 +7,19 @@ mix.postCss('resources/css/app.css', 'public/css', [
     require('tailwindcss'),
     require('autoprefixer'),
 ]);
+
+mix.browserSync({
+    port: 8001,
+    notify: false,
+    watch: true,
+    proxy: {
+        target: "http://localhost:8000/",
+        ws: true,
+    },
+    files: [
+        'public/js/**/*',
+        'public/css/**/*',
+        'public/**/*.+(html|php)',
+        'resources/views/**/*.php'
+    ],
+});
