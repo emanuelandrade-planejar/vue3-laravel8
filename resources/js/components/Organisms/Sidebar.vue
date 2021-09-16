90b1a19604d4bb580f9968d5b9e78b455e284451
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-16 16:46:45 -0300

    Aumentando espaço entre logo e perfil

--- resources/js/components/Organisms/Sidebar.vue
+++ resources/js/components/Organisms/Sidebar.vue
@@ -2,7 +2,7 @@
     <div class="mx-auto w-4/5 mt-8 mb-4">
         <h3 class="font-bold text-2xl">Siplan Cloud</h3>
   
-        <div class="mx-auto w-full mb-8 flex flex-row items-center">
+        <div class="mx-auto w-full mb-8 mt-1.5 flex flex-row items-center">
             <div class="mr-2">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
             </div>
