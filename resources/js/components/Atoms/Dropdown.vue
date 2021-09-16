dad343c08b4f8b6ce7928e96c0ac225ed95ee8e6
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-16 16:46:21 -0300

    Aumentando largura do dropdown

--- resources/js/components/Atoms/Dropdown.vue
+++ resources/js/components/Atoms/Dropdown.vue
@@ -4,7 +4,7 @@
             <slot name="anchor" />
         </div>
 
-        <div v-if="isOpen" class="absolute right-0 -mr-2 mt-1 min-w-60 bg-white border border-gray-200 px-4 py-2 rounded shadow-lg">
+        <div v-if="isOpen" class="absolute right-0 -mr-2 mt-1 min-w-72 bg-white border border-gray-200 px-4 py-2 rounded shadow-lg">
             <strong class="block text-gray-800 border-b border-gray-300 mb-3">{{ title }}</strong>
 
             <slot name="content" />
