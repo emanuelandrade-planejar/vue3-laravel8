0791a77fd3e60f40f8eff019946c20b1d9a4ffe5
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-16 16:45:44 -0300

    Adicionando ícone ao input de texto

--- resources/js/components/Atoms/form/InputText.vue
+++ resources/js/components/Atoms/form/InputText.vue
@@ -2,17 +2,33 @@
     <div class="my-4">
         <label :for="id" class="block text-gray-700 font-bold">{{ label }}</label>
 
-        <input
-            v-model="proxyValue"
-            :id="id" 
-            :name="name" 
-            :type="type" 
-            class="rounded shadow border border-gray-400 h-8 focus:border-indigo-600" 
+        <div 
+            class="w-full flex items-center justify-between p-1 rounded shadow border border-gray-400 h-8 focus-within:border-indigo-600"
+            :class="{
+                'flex-row': iconPosition === 'left',
+                'flex-row-reverse': iconPosition === 'right'
+            }"
+            @click="focusOnINput"
         >
+            <div v-if="!!$slots?.icon?.()" class="icon h-full flex items-center text-gray-800" @click="focusOnINput">
+                <slot name="icon" />
+            </div>
+
+            <input
+                ref="inputRef"
+                v-model="proxyValue"
+                :id="id" 
+                :name="name"
+                :type="type" 
+                class="w-full border-none h-full focus:ring-0"            
+            >
+        </div>
     </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+
 import { useInputModel } from "../../../useInputModel"
 
 export default {
@@ -24,19 +40,29 @@ export default {
             type: String,
             default: "text"
         },
-        modelValue: String
+        iconPosition: {
+            type: String,
+            default: "left"
+        },
+        modelValue: String,
     },
     emits: ['update:modelValue'],
     setup(props) {
         const proxyValue = useInputModel(props)
 
-        return {
-            proxyValue
+        const inputRef = ref()
+
+        function focusOnINput() {
+            inputRef.value?.focus()
         }
+
+        return { proxyValue, inputRef, focusOnINput }
     }
 }
 </script>
 
 <style>
-
+.icon svg {
+    height: 80%;
+}
 </style>
