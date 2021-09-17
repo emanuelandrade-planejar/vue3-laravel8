b03f95d70b7048303001c9dfc42952a9b0ced69e
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-17 15:53:17 -0300

    Limpando HTML e adicionando variavel global Current Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,25 +15,10 @@
         <script>
             window.__pageProps = @json($props);
             window.__globalData = @json($global);
+            window.__currentPage = @json(Route::current()->getName());
         </script>
 
-        <div id="app">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <home />
-        </div>
+        <div id="app"></div>
 
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
