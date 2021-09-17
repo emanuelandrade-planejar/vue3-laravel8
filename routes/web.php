1387cb19bdbad37a8cca39e120b4a699d4b5fef2
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-17 15:53:26 -0300

    Adicionando rota de Sistemas

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+$globalData = [
+    "systems" => [
+        ["name" => "Base", "description" => "Sitema Base"], 
+        ["name" => "Frotas", "description" => "Sitema de Frotas"], 
+        ["name" => "Almoxarifado", "description" => "Sitema de Almoxarifado"],
+        ["name" => "Importação e Exportação", "description" => "Sitema de Importação e Exportação"], 
+    ],
+    "user" => [ "nome" => "José Silva", "idade" => 34 ]
+];
+
+Route::get('/', function () use ($globalData) {
     return view('welcome', [
         "props" => [
             "list" => [1,2,3,4,5,6]
         ],
-        "global" => [
-            "systems" => ["Base", "Importação e Exportação", "Frotas", "Almoxarifado"],
-            "user" => [ "nome" => "José Silva", "idade" => 34 ]
-        ]
+        "global" => $globalData 
     ]);
-});
+})->name('Home');
+
+Route::get('/sistemas', function () use ($globalData) {
+    return view('welcome', [
+        "props" => [
+            "list" => [1,2,3,4,5,6]
+        ],
+        "global" => $globalData
+    ]);
+})->name('Systems');
