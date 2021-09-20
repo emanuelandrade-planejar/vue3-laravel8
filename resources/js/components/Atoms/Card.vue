651a00c847d262e22dfb3929524afdefa3d9697c
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-20 17:21:22 -0300

    Adicionando footer ao Card

--- resources/js/components/Atoms/Card.vue
+++ resources/js/components/Atoms/Card.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="bg-white shadow-lg rounded overflow-hidden">
+    <div class="flex flex-col justify-between bg-white shadow-lg rounded overflow-hidden">
         <header class="w-full py-3 px-4 text-white font-bold bg-indigo-600"><slot name="header" /></header>
 
-        <div class="py-3 px-4"><slot /></div>
+        <div class="py-3 px-4 pr-10"><slot /></div>
+
+        <footer v-if="!!$slots?.footer?.()" class="w-full py-3 px-4 bg-gray-100 border-t border-gray-300 border-opacity-70"><slot name="footer" /></footer>
     </div>
 </template>
 
