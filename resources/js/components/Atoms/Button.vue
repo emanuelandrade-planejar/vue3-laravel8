4b869c13c72745202e964227f572a1a6fdb81040
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-20 17:20:55 -0300

    Alinhando ao centro o texto do botão

--- resources/js/components/Atoms/Button.vue
+++ resources/js/components/Atoms/Button.vue
@@ -1,7 +1,7 @@
 <template>
     <component 
         :is="as" 
-        class="flex items-center h-8 px-4 rounded bg-indigo-700 text-white transition-colors ease-in-out duration-150 hover:bg-indigo-800"
+        class="flex items-center justify-center h-8 px-4 rounded bg-indigo-700 text-white transition-colors ease-in-out duration-150 hover:bg-indigo-800"
     >
         <span>
             <slot />
