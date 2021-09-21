44a00b48660fdd91653a32df85309be35384df54
emanuel.andrade <emanuel.andrade@planejarjf.com.br>
2021-09-21 16:34:50 -0300

    Componente de paginação finalizado

--- resources/js/components/Atoms/Pagination.vue
+++ resources/js/components/Atoms/Pagination.vue
@@ -5,50 +5,22 @@
         </div>
 
         <div class="flex flex-row items-center justify-between">
-            <button class="px-1 py-2 rounded hover:bg-gray-300"><ChevronLeftIcon class="w-4 h-4 text-gray-800" /></button>
+            <button class="px-1 py-2 rounded hover:bg-gray-300" @click="data.currentPage = 1"><ChevronDoubleLeftIcon class="w-4 h-4 text-gray-800" /></button>
+
+            <button 
+                class="px-1 py-2 rounded hover:bg-gray-300"
+                @click="data.currentPage = data.currentPage - 1 < 1 ? 1 : data.currentPage - 1"
+            >
+                <ChevronLeftIcon class="w-4 h-4 text-gray-800" />
+            </button>
             
 
             <nav>
                 <ul class="flex flex-row items-center justify-between gap-1 mx-1">
-                    <template v-for="page in data.totalPages" :key="page">
-                        <li  v-if="page <= 2" >
-                            <button                                
-                                class="px-2 py-1 rounded"
-                                :class="[
-                                    page === data.currentPage ? 'text-white bg-indigo-700 hover:bg-indigo-800' : 'text-gray-800 hover:bg-gray-300'
-                                ]"
-                            >
-                                {{ page }}
-                            </button>
-                        </li>
-                    </template>
-                    
-                    <li>
-                        <span class="px-2 py-1">...</span>
-                    </li>
-
-
-                    <template v-for="page in data.totalPages" :key="page">
-                        <li v-if="(page >= data.currentPage - 1 && page > 2) && (page <= data.currentPage + 1 && page < data.totalPages - 1)" >
+                    <template v-for="page in preparePageIndication(data)" :key="page">
+                        <li>
                             <button
-                                class="px-2 py-1 rounded"
-                                :class="[
-                                    page === data.currentPage ? 'text-white bg-indigo-700 hover:bg-indigo-800' : 'text-gray-800 hover:bg-gray-300'
-                                ]"
-                            >
-                                {{ page }}
-                            </button>
-                        </li>
-                    </template>
-
-                    <li>
-                        <span class="px-2 py-1">...</span>
-                    </li>
-
-                    <template v-for="page in data.totalPages" :key="page">  
-                        <li v-if="page >= data.totalPages - 1" >                      
-                            <button 
-                                class="px-2 py-1 rounded" 
+                                class="w-8 py-1 rounded"
                                 :class="[
                                     page === data.currentPage ? 'text-white bg-indigo-700 hover:bg-indigo-800' : 'text-gray-800 hover:bg-gray-300'
                                 ]"
@@ -60,23 +32,69 @@
                 </ul>
             </nav>
 
-            <button class="px-1 py-2 rounded hover:bg-gray-300"><ChevronRightIcon class="w-4 h-4 text-gray-800" /></button>
+            <button 
+                class="px-1 py-2 rounded hover:bg-gray-300" 
+                @click="data.currentPage = data.currentPage + 1 > data.totalPages ? data.totalPages : data.currentPage + 1"
+            >
+                <ChevronRightIcon class="w-4 h-4 text-gray-800" />
+            </button>
+
+            <button class="px-1 py-2 rounded hover:bg-gray-300" @click="data.currentPage = data.totalPages"><ChevronDoubleRightIcon class="w-4 h-4 text-gray-800" /></button>
         </div>
     </section>
 </template>
 
 <script>
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/outline'
+import { reactive } from '@vue/reactivity'
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/vue/outline'
 
 export default {
-    components: { ChevronLeftIcon, ChevronRightIcon },
+    components: { ChevronLeftIcon, ChevronRightIcon, ChevronDoubleLeftIcon, ChevronDoubleRightIcon },
     setup() {
-        const data = {
+        const data = reactive({
             currentPage: 6,
             totalPages: 10,
+        })
+
+        function preparePageIndication(data) {
+            const totalPages = data.totalPages
+            const currentPage = data.currentPage
+
+            let pages = Array(3)
+
+            for (let page = 1; page <= totalPages; page++) {
+                const isCurrentePage = page === currentPage
+
+                const isLeftOfCurrentePage = (page === currentPage - 1)
+                const isRightOfCurrentePage = (page === currentPage + 1)
+
+                if(isLeftOfCurrentePage) {
+                    pages[0] = page
+                }
+
+                if(isCurrentePage) {
+                    pages[1] = page
+                }
+
+                if(isRightOfCurrentePage) {
+                    pages[2] = page
+                }
+            }
+            
+            for (let page = 1; page <= totalPages; page++) {
+                if(!pages[0] || !pages[2]) {
+                    if(page === pages[2] + 1)
+                        pages = [...pages, page]                        
+                    
+                    if(page === pages[0] - 1)
+                        pages = [page, ...pages]                        
+                }
+            }
+
+            return pages.filter(page => !!page)
         }
 
-        return { data }
+        return { data, preparePageIndication }
     }
 }
 </script>
